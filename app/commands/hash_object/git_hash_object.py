6cb13f17a2c6edc716fc96f3921d582f9ca761a9
Gerhard <gerhard-richard.gruber@outlook.at>
2025-05-04 14:51:26 +0200

    now hash of implementation matches git

--- app/commands/hash_object/git_hash_object.py
+++ app/commands/hash_object/git_hash_object.py
@@ -14,17 +14,16 @@ def git_hash_object(args):
     with open(file_to_hash, 'r') as f:
         file_content = f.read()
 
-    file_length = file_content.__sizeof__()
+    file_length = len(file_content)
 
-    object_content = f"{object_type} {file_length}\0{file_content}".encode()
-    object_content = zlib.compress(object_content)
-    object_hash = sha1(object_content).hexdigest()
+    object_header = f"{object_type} {file_length}\x00".encode('utf-8')
+    object_hash = sha1(object_header + file_content.encode()).hexdigest()
 
     if write_to_database:
         object_path = f"{object_hash[:2]}/{object_hash[2:]}"
         object_repo_path = git_repo.joinpath("objects").joinpath(object_path)
         object_repo_path.parent.mkdir(parents=True, exist_ok=True)
         with open(object_repo_path, 'wb') as f:
-            f.write(object_content)
+            f.write(zlib.compress(object_header + file_content.encode()))
 
     print(object_hash)
