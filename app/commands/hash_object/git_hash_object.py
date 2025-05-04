2fb89e264c64922eae4a009ce02e0b408aada2e3
Gerhard <gerhard-richard.gruber@outlook.at>
2025-05-04 16:43:07 +0200

    refactor to have reusable functions for writing objects

--- app/commands/hash_object/git_hash_object.py
+++ app/commands/hash_object/git_hash_object.py
@@ -2,6 +2,7 @@ import zlib
 from hashlib import sha1
 
 from app.utils.find_git_repo import get_current_git_repo
+from app.utils.git_object import create_object, calculate_object_hash, write_object
 
 
 def git_hash_object(args):
@@ -16,14 +17,10 @@ def git_hash_object(args):
 
     file_length = len(file_content)
 
-    object_header = f"{object_type} {file_length}\x00".encode('utf-8')
-    object_hash = sha1(object_header + file_content.encode()).hexdigest()
+    _object = create_object(object_type, file_length, file_content)
+    object_hash = calculate_object_hash(_object)
 
     if write_to_database:
-        object_path = f"{object_hash[:2]}/{object_hash[2:]}"
-        object_repo_path = git_repo.joinpath("objects").joinpath(object_path)
-        object_repo_path.parent.mkdir(parents=True, exist_ok=True)
-        with open(object_repo_path, 'wb') as f:
-            f.write(zlib.compress(object_header + file_content.encode()))
+        write_object(_object, object_hash, git_repo)
 
     print(object_hash)
