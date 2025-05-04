95276984689eb390491b6fd981cd33ee72a59cb2
Gerhard <gerhard-richard.gruber@outlook.at>
2025-05-04 13:59:21 +0200

    add utility functions to find the current git repository

---
+++ app/utils.py
@@ -0,0 +1,46 @@
+import os
+from pathlib import Path
+import re
+
+
+def is_git_directory(path: str | Path):
+    c_path = Path(path)
+    objects_dir = c_path.joinpath('objects')
+    refs_dir = c_path.joinpath('refs')
+    head_file = c_path.joinpath('HEAD')
+
+    if head_file.exists():
+        with open(head_file) as f:
+            head_content = f.read().strip()
+
+        # is_syntax_ok_head = len(re.search(r"ref:\s+refs/heads/.*", head_content).groups()) > 0 # TODO: when is HEAD syntactically correct, for now just set to true is exists
+        is_syntax_ok_head = True
+    else:
+        is_syntax_ok_head = False
+
+    if is_syntax_ok_head and objects_dir.exists() and refs_dir.exists():
+        return True
+
+    return False
+
+
+def find_git_repo(path: str | Path):
+    c_path = Path(path)
+    git_dir = [c_dir for c_dir in c_path.iterdir() if c_dir.is_dir() and c_dir.name.startswith('.') and is_git_directory(c_dir)]
+    if len(git_dir) > 0:
+        return git_dir[0]
+    else:
+        if str(c_path.parent) != str(c_path):
+            return find_git_repo(c_path.parent)
+        else:
+            return None
+
+
+
+def get_current_git_repo() -> Path:
+    repo_path = os.getenv("GIT_DIR")
+
+    if repo_path is None:
+        repo_path = find_git_repo(os.getcwd())
+
+    return Path(repo_path)
